f51fcaf36ab13e120883129eb6479fb0c3d42e52
Tom Hanna <tlhanna@uh.edu>
2025-12-18 19:19:27 -0600

    starting non-vdem data

---
+++ non-vdem-data-monadic.R
@@ -0,0 +1,37 @@
+# load directed dyad MIDS data
+
+
+
+# convert directional dyad variables to monadic variables
+
+
+
+# load trade data from RED dataset
+
+
+# convert trade data to monadic variables
+
+
+# load alliance data from ATOP dataset
+
+
+# convert alliance data to monadic variables
+
+
+# load Global Dataset on Political Leaders, 1945-2020
+
+# load ALBA data
+
+
+# merge all monadic data into monadic panel
+
+# save monadic panel data
+
+
+
+
+
+
+
+
+
